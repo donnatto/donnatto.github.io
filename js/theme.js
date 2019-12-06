ca6f96a927a7ee2c42ab249deec3d9276e70d1e0
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-06 00:51:00 -0500

    portfolio page commit

---
+++ js/theme.js
@@ -0,0 +1,30 @@
+(function($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+
+	$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+
+
+})(jQuery);
